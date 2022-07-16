4e3175a690668d6ff9ca28274b645e9b14b06a38
Sean-Miningah <miningahsean@gmail.com>
2022-07-16 21:30:02 +0300

    Refactoring the fucntioal test to check input

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        # wer use POST.get('item_text', '') to cater for get request
+        'new_item_text': request.POST.get('item_text', '')
+    })
 
